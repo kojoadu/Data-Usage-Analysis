601a8a32d4dd01dab3dfa154cd7adc88ebe13551
kojoadu <kojoadu@live.com>
2024-06-02 20:07:21 +0000

    upload size

--- pages/FBB.py
+++ pages/FBB.py
@@ -8,6 +8,8 @@ from modules.util import get_max_date, get_min_date
 
 st.title("FTTX Data Usage Analysis")
 
+st.set_option('server.maxUploadSize', 3072)  # Set maximum upload size to 1 GB
+
 # Function to get ISP from ip-api.com asynchronously
 async def get_isp(session, ip):
     try:
